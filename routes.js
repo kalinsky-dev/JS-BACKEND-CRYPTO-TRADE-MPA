0640aa35ae2a1cc8ba9730eae985e1175b0331ab
Kalin Hristov <kalin0169@gmail.com>
2023-02-10 15:42:04 +0200

    add Auth Controller

--- routes.js
+++ routes.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const homeController = require('./controllers/homeController');
+const authController = require('./controllers/authController');
+
 //TODO: add routes
+
 router.use(homeController);
+router.use(authController);
 
 module.exports = router;
