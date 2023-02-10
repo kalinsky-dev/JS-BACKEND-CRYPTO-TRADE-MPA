517f60b529b0a84af6e6e6a8c4cf19171c721494
Kalin Hristov <kalin0169@gmail.com>
2023-02-10 14:00:17 +0200

    Add view engine: express-handlebars
    Register with express, add views folder, add main layout

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
+
 const routes = require('./routes');
 
 const app = express();
 
+app.engine('hbs', handlebars.engine({ extname: 'hbs' }));
+app.set('view engine', 'hbs');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
 app.use(routes);
 
 app.listen(3000, () => console.log('Server is running on port 3000...'));
