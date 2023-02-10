83c50736af6e287b0b06ff0858f0656aa9d4ac00
Kalin Hristov <kalin0169@gmail.com>
2023-02-10 15:02:07 +0200

    set Strict Query

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 
 const routes = require('./routes');
 
@@ -13,6 +13,7 @@ app.use('/static', express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(routes);
 
-mongoose.connect(`mongodb://127.0.0.1:27017/crypto`)
+mongoose.set('strictQuery', false);
+mongoose.connect(`mongodb://127.0.0.1:27017/crypto`);
 
 app.listen(3000, () => console.log('Server is running on port 3000...'));
