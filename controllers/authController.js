0bdcc465f6d7b9ff44329de6e8d6fe0674880a8e
Kalin Hristov <kalin0169@gmail.com>
2023-02-10 15:58:26 +0200

    Add login and register pages

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,8 +4,17 @@ router.get('/register', (req, res) => {
   res.render('auth/register');
 });
 
+router.post('/register', (req, res) => {
+  const { username, email, password, repeatPassword } = req.body;
+  res.render('auth/register');
+});
+
 router.get('/login', (req, res) => {
   res.render('auth/login');
+});
+
+router.post('/login', (req, res) => {
+  const { email, password } = req.body;
 });
 
 module.exports = router;
