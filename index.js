154a51d4ce3abe75f076b4ad67de166a5dbd76ed
Kalin Hristov <kalin0169@gmail.com>
2023-02-10 14:54:22 +0200

    Connect mongooseDB

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
+const mongoose=require('mongoose');
 
 const routes = require('./routes');
 
@@ -11,5 +12,7 @@ app.set('view engine', 'hbs');
 app.use('/static', express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(routes);
+
+mongoose.connect(`mongodb://127.0.0.1:27017/crypto`)
 
 app.listen(3000, () => console.log('Server is running on port 3000...'));
