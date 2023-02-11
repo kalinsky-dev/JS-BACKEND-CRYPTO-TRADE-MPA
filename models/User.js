5449b7a8b4cfbd8f8ec75158cc8b783e8889c28e
Kalin Hristov <kalin0169@gmail.com>
2023-02-11 10:32:48 +0200

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    virtuals: {
+      repeatPassword: {
+        set(value) {
+          if (this.password !== value) {
+            throw new mongoose.Error('Password missmatch!');
+          }
+        },
+      },
+    },
+  }
+);
+
+// userSchema.virtual('repeatPassword').set
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
